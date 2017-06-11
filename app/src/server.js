6174d99e0027db35353153e365c24f2e20eeda95
priya3096 <priyamandal1974@gmail.com>
2017-06-11 13:11:18 +0530

    HTML files

--- app/src/server.js
+++ app/src/server.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 
 //your routes here
-app.get('/', function (req, res) {
-    res.send("Hello World!");
+var root = process.cwd();
+app.get('/login', function (req, res) {
+    res.sendFile('html/login.html',{root});
 });
+app.get('/register', function (req, res) {
+    res.sendFile('html/register.html',{root});
+});
+app.get('/home', function (req, res) {
+    res.send("Welcome to home page! You may enter the required details..");
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
